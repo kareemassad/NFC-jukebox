bc36aafed745e257d2cba8ba36bbf4f3e43a8b65
Kareem Assad <kareemassad5@gmail.com>
2020-07-20 14:37:32 -0400

    working with authorization

--- testSpotifyAPI.py
+++ testSpotifyAPI.py
@@ -1,13 +1,16 @@
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 import spotipy
-import sys
 import pprint
 
 username = '22wtiqz6ow2wcjaoopq5k4vyy'
+scope = 'user-read-playback-state'
 
-client_credentials_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+#client_credentials_manager = SpotifyClientCredentials()
+#sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-#sp.trace = True
-user = sp.user(username)
-pprint.pprint(user)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+sp.trace = True
+#user = sp.user(username)
+allDevices = sp.devices()
+pprint.pprint(allDevices)

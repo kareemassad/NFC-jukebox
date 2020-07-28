d32877d24b39a31469231c623137e0340a903715
kareemassad <kareemassad5@gmail.com>
2020-07-28 12:26:06 -0400

    the getAlbumTracks method is functional but is no longer needed for the project. Added a method that finds a device ID for playback

--- Test/testSpotifyAPI.py
+++ Test/testSpotifyAPI.py
@@ -9,9 +9,22 @@ from json.decoder import JSONDecodeError
 #TODO add spotify cliendID, clientSecret, and redirectURI into env variables
 
 def playTrack(trackURI):
-    return spotifyObject.start_playback("31876612233caf235184b622d80c84b51b39cc36", None ,trackURI)
+    """This method start's playback on a 
+
+    Args:
+        trackURI (String): [Contains the URI of the album/artist/playlist]
+    """
+    spotifyObject.start_playback("31876612233caf235184b622d80c84b51b39cc36", trackURI ,None)
 
 def getAlbumTracks(albumURI):
+    """This method takes an album URI and returns a list containing all of the URIs for the tracks in it. No longer required with the latest spotify update
+
+    Args:
+        albumURI (String): [A spotify album URI]
+
+    Returns:
+        tracks(list of strings): [Contains the album's tracks as spotify URI links]
+    """
     i = 0
     n = 50
     result = spotifyObject.album_tracks(albumURI, n, 0 ,None)
@@ -23,9 +36,31 @@ def getAlbumTracks(albumURI):
         tracks.append(trackLinks)
     return tracks
 
-#username from spotify main page
+def findDeviceID():
+    """ This method finds all devices available to play on a spotify account and returns the device to be used for playback. It always returns the ID of the Amazon Echo unless it is unavailable.
+
+    Returns:
+        deviceID(String): Returns the ID of the Amazon Echo if it is available.
+        devices[0](String): If it isn't, returns the first other option.
+    """
+    #get all active devices
+    result = spotifyObject.devices()
+    print(json.dumps(result, sort_keys=True, indent=4))
+
+    #Store all device ID's found in a list
+    devices = []
+    for n in range(len(result['devices'])):
+        deviceID = result['devices'][n]['id']
+        devices.append(deviceID)
+
+        #if spotify speaker exists, play on it. Otherwise, play on whatever.
+        if(deviceID == "31876612233caf235184b622d80c84b51b39cc36s"):
+            return deviceID
+    #Play on whatever is available first.
+    print(devices[0])
+    return devices[0]
+
 username = '22wtiqz6ow2wcjaoopq5k4vyy'
-#changes based on what you want to do, read documentation
 scope = 'user-read-private user-modify-playback-state user-read-playback-state'
 
 #authentication
@@ -38,23 +73,11 @@ except (AttributeError, JSONDecodeError):
 #create a spotify object
 spotifyObject = spotipy.Spotify(auth=token)
 
-#what device is playing
-#devices = spotifyObject.devices()
-#print(json.dumps(devices, sort_keys=True, indent=4))
-#deviceID = devices['devices'][1]['id']
-#print(deviceID)
 
 #get user information
 user = spotifyObject.current_user()
-displayName = user['display_name']
-follower = user['followers']['total']
-#print(user, displayName, follower)
 
 #give album
-albumURI = 'spotify:album:37rNuexqEXWeSIOiJtn3A9'
-albumtracks = getAlbumTracks(albumURI)
-tracks = print(getAlbumTracks(albumURI))
+albumURI = 'spotify:album:05J8PFXdYKeYNb8YjqqJYr'
 
-#Play a track
-#trackURI = ['spotify:track:6SJLngO5LfdEldlJd1MWCs']
-playTrack(tracks)
+findDeviceID()

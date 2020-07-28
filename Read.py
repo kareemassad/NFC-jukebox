ee8dea5b4392fa78a91fa6bb5b549bed388940b5
kareemassad <kareemassad5@gmail.com>
2020-07-28 12:59:47 -0400

    fixing formatting issues and minor improvements

--- Read.py
+++ Read.py
@@ -3,48 +3,122 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import csv
-from extractURI import getSpotifyURI
+
 
 def getSpotifyURI(ID):
-        """
-        Given an ID type integer, return the corresponding spotify URI
+    """
+    Given an ID type integer, return the corresponding spotify URI
 
-        Args:
-            ID ([Integer]): [A unique ID given directly from each unique NFC tag]
+    Args:
+        ID ([Integer]): [A unique ID given directly from each unique NFC tag]
 
-        Returns:
-            URI ([String]): [A spotify URI pulled from a .csv file that directly coressponds a specific ID]
-        """
-        
-        file = open('spotifyURICollection.csv', encoding='utf-8-sig')
-        csv_file = csv.DictReader(file)
-        for row in csv_file:
-                #print(row['ID'])
-                #DONE: concantinate ID to a string 
-                if(str(ID) == row['ID']):
-                        URI = row['URI']
-                        print(URI)
-                        return URI
-        #Base Case
-        return "no match found"
+    Returns:
+        URI ([String]): [A spotify URI pulled from a .csv file that directly coressponds a specific ID]
+    """
 
-def playSpotify(URI):
-        return 
+    file = open('spotifyURICollection.csv', encoding='utf-8-sig')
+    csv_file = csv.DictReader(file)
+    for row in csv_file:
+        # print(row['ID'])
+        # DONE: concantinate ID to a string
+        if(str(ID) == row['ID']):
+            URI = row['URI']
+            print(URI)
+            return URI
+    # Base Case
+    return "no match found"
+
+
+def playSpotify(contextURI, deviceID):
+    """This method takes a context URI and a Device ID then plays said URI on the given Device
+
+    Args:
+        contextURI (String): Contains the URI of the album/artist/playlist
+        deviceID (String): Contains the device ID meant for playback
+    """
+    # "31876612233caf235184b622d80c84b51b39cc36"
+    spotifyObject.start_playback(deviceID, contextURI, None)
+
+
+def getAlbumTracks(albumURI):
+    """This method takes an album URI and returns a list containing all of the URIs for the tracks in it. No longer required with the latest spotify update
+
+    Args:
+        albumURI (String): [A spotify album URI]
+
+    Returns:
+        tracks(list of strings): [Contains the album's tracks as spotify URI links]
+    """
+    i = 0
+    n = 50
+    result = spotifyObject.album_tracks(albumURI, n, 0, None)
+
+    # append all track URIs into list tracks
+    tracks = []
+    for n in range(len(result['items'])):
+        trackLinks = result['items'][n]['uri']
+        tracks.append(trackLinks)
+    return tracks
+
+
+def findDeviceID():
+    """ This method finds all devices available to play on a spotify account and returns the device to be used for playback. It always returns the ID of the Amazon Echo unless it is unavailable.
+
+    Returns:
+        deviceID(String): Returns the ID of the Amazon Echo if it is available.
+        devices[0](String): If it isn't, returns the first other option.
+    """
+    # get all active devices
+    result = spotifyObject.devices()
+    #print(json.dumps(result, sort_keys=True, indent=4))
+
+    # Store all device ID's found in a list
+    devices = []
+    for n in range(len(result['devices'])):
+        deviceID = result['devices'][n]['id']
+        devices.append(deviceID)
+
+        # if spotify speaker exists, play on it. Otherwise, play on whatever.
+        if(deviceID == "31876612233caf235184b622d80c84b51b39cc36"):
+            return deviceID
+    # Play on whatever is available first.
+    # print(devices[0])
+    return devices[0]
+
+
+username = '22wtiqz6ow2wcjaoopq5k4vyy'
+scope = 'user-read-private user-modify-playback-state user-read-playback-state'
+
+# authentication
+# remember to add cache to .gitignore
+try:
+    token = util.prompt_for_user_token(username, scope)
+except (AttributeError, JSONDecodeError):
+    os.remove(f".cache-{username}")
+    token = util.prompt_for_user_token(username, scope)
+
+# create a spotify object
+spotifyObject = spotipy.Spotify(auth=token)
+# give album
+albumURI = 'spotify:album:7n3QJc7TBOxXtlYh4Ssll8'
+# get device to play on
+deviceID = findDeviceID()
+# play the album
+playSpotify(albumURI, deviceID)
 
 reader = SimpleMFRC522()
 
 try:
-        print("Place your tag to be read!")
-        id, text = reader.read()
-        #id represents the unique serial number of each tag
-        #access data in URI variable
-        getSpotifyURI(id)
-        #pass URI variable in new function that interfaces with spotify
-        #TODO: Create a new function that plays a song on spotify based on the URI 
+    print("Place your tag to be read!")
+    id, text = reader.read()
+    # id represents the unique serial number of each tag
+    # access data in URI variable
+    getSpotifyURI(id)
+    # pass URI variable in new function that interfaces with spotify
+    # TODO: Create a new function that plays a song on spotify based on the URI
 
-
-        print(id)
-        #text represents the text written to the tag
-        #print(text)
+    print(id)
+    # text represents the text written to the tag
+    # print(text)
 finally:
-        GPIO.cleanup()
+    GPIO.cleanup()
--- Test/testSpotifyAPI.py
+++ Test/testSpotifyAPI.py
@@ -6,15 +6,19 @@ import webbrowser
 import spotipy.util as util
 from json.decoder import JSONDecodeError
 
-#TODO add spotify cliendID, clientSecret, and redirectURI into env variables
+# TODO add spotify cliendID, clientSecret, and redirectURI into env variables
 
-def playTrack(trackURI):
-    """This method start's playback on a 
+
+def playSpotify(contextURI, deviceID):
+    """This method takes a context URI and a Device ID then plays said URI on the given Device
 
     Args:
-        trackURI (String): [Contains the URI of the album/artist/playlist]
+        contextURI (String): Contains the URI of the album/artist/playlist
+        deviceID (String): Contains the device ID meant for playback
     """
-    spotifyObject.start_playback("31876612233caf235184b622d80c84b51b39cc36", trackURI ,None)
+    # "31876612233caf235184b622d80c84b51b39cc36"
+    spotifyObject.start_playback(deviceID, contextURI, None)
+
 
 def getAlbumTracks(albumURI):
     """This method takes an album URI and returns a list containing all of the URIs for the tracks in it. No longer required with the latest spotify update
@@ -27,14 +31,15 @@ def getAlbumTracks(albumURI):
     """
     i = 0
     n = 50
-    result = spotifyObject.album_tracks(albumURI, n, 0 ,None)
+    result = spotifyObject.album_tracks(albumURI, n, 0, None)
 
-    #append all track URIs into list tracks
+    # append all track URIs into list tracks
     tracks = []
     for n in range(len(result['items'])):
         trackLinks = result['items'][n]['uri']
         tracks.append(trackLinks)
     return tracks
+
 
 def findDeviceID():
     """ This method finds all devices available to play on a spotify account and returns the device to be used for playback. It always returns the ID of the Amazon Echo unless it is unavailable.
@@ -43,41 +48,40 @@ def findDeviceID():
         deviceID(String): Returns the ID of the Amazon Echo if it is available.
         devices[0](String): If it isn't, returns the first other option.
     """
-    #get all active devices
+    # get all active devices
     result = spotifyObject.devices()
-    print(json.dumps(result, sort_keys=True, indent=4))
+    #print(json.dumps(result, sort_keys=True, indent=4))
 
-    #Store all device ID's found in a list
+    # Store all device ID's found in a list
     devices = []
     for n in range(len(result['devices'])):
         deviceID = result['devices'][n]['id']
         devices.append(deviceID)
 
-        #if spotify speaker exists, play on it. Otherwise, play on whatever.
-        if(deviceID == "31876612233caf235184b622d80c84b51b39cc36s"):
+        # if spotify speaker exists, play on it. Otherwise, play on whatever.
+        if(deviceID == "31876612233caf235184b622d80c84b51b39cc36"):
             return deviceID
-    #Play on whatever is available first.
-    print(devices[0])
+    # Play on whatever is available first.
+    # print(devices[0])
     return devices[0]
+
 
 username = '22wtiqz6ow2wcjaoopq5k4vyy'
 scope = 'user-read-private user-modify-playback-state user-read-playback-state'
 
-#authentication
+# authentication
+# remember to add cache to .gitignore
 try:
     token = util.prompt_for_user_token(username, scope)
 except (AttributeError, JSONDecodeError):
     os.remove(f".cache-{username}")
     token = util.prompt_for_user_token(username, scope)
 
-#create a spotify object
+# create a spotify object
 spotifyObject = spotipy.Spotify(auth=token)
-
-
-#get user information
-user = spotifyObject.current_user()
-
-#give album
-albumURI = 'spotify:album:05J8PFXdYKeYNb8YjqqJYr'
-
-findDeviceID()
+# give album
+albumURI = 'spotify:album:7n3QJc7TBOxXtlYh4Ssll8'
+# get device to play on
+deviceID = findDeviceID()
+# play the album
+playSpotify(albumURI, deviceID)
